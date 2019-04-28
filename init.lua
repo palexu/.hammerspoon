2e32269fb28b5e116efa7630775b7ad6587e915f
xujunyu <junyu.x@shulidata.com>
2019-04-28 10:30:29 +0800

    add wifi mutex

--- init.lua
+++ init.lua
@@ -17,3 +17,33 @@ hs.hotkey.bind({"alt"}, "T", open("iTerm"))
 hs.hotkey.bind({"alt"}, "S", open("Sublime Text"))
 hs.hotkey.bind({"alt"}, "I", open("IntelliJ IDEA"))
 hs.hotkey.bind({"alt"}, "M", open("NeteaseMusic"))
+
+--- 切换wifi
+
+local workWifi = 'shuli-wifi'
+local outputDeviceName = 'Built-in Output'
+
+local workUid = "A18D1BFD-4CD6-4A64-9677-EEAE623F1839"
+local homeUid = "7500A4F7-72BD-4ABB-9D31-3934BBE13F39"
+
+function ssidChangedCallback() -- 回调
+    ssid = hs.wifi.currentNetwork() -- 获取当前 WiFi ssid
+    local currentOutput = hs.audiodevice.current(false)
+    if (ssid ~= nil) then
+        if (ssid == workWifi) then
+            uid = workUid -- 公司的位置 uid，后面会讲
+            hs.notify.new({title="位置", informativeText="位置切换到公司"}):send() -- 发出通知
+            if (currentOutput.name == outputDeviceName) then
+                hs.audiodevice.findDeviceByName(outputDeviceName):setOutputMuted(true)
+                hs.notify.new({title="声音", informativeText="在公司静音"}):send() -- 发出通知
+            end
+        else
+            uid = homeUid
+            hs.notify.new({title="位置", informativeText="位置切换到家里"}):send()
+        end
+        os.execute("scselect " ..uid .." > /dev/null") -- 切换网络位置
+    end
+end
+
+wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
+wifiWatcher:start() -- 开始监控
